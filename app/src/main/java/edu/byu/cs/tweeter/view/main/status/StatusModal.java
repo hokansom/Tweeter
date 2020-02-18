610b644ff23e1693fdac5979f16305d09ac03651
Morgan Davis <morgan.hokanson@gmail.com>
2020-02-18 10:31:35 -0700

    in progress

--- app/src/main/java/edu/byu/cs/tweeter/view/main/status/StatusModal.java
+++ app/src/main/java/edu/byu/cs/tweeter/view/main/status/StatusModal.java
@@ -6,13 +6,26 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
-import edu.byu.cs.tweeter.R;
+import androidx.annotation.Nullable;
 
-public class StatusModal {
+import edu.byu.cs.tweeter.R;
+import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.presenter.StatusPresenter;
+
+public class StatusModal implements StatusPresenter.View {
+
+    private StatusPresenter presenter;
+    private User user;
+
+
 
     public void showPopupWindow(final View view){
+//        presenter = new StatusPresenter(view);
+
         LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(view.getContext().LAYOUT_INFLATER_SERVICE);
         View modalView = inflater.inflate(R.layout.fragment_new_status, null);
+
+
 
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
@@ -22,9 +35,6 @@ public class StatusModal {
         final PopupWindow popUpWindow = new PopupWindow(modalView, width, height, focusable);
 
         popUpWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-
-
 
     }
 }
